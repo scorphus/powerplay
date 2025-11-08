0f4c9f5e2149d73ca574e4a2e2d2f1f26ddcc293
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2025-11-08 16:43:39 +0100

    src/main: Add Vercel Analytics

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+
+inject()
 
 const app = createApp(App)
 app.use(createPinia())
